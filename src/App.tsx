fcfca12d9e33f10384176c3acf8fe787f1acb715
jicking bebiro <7297029+jicking@users.noreply.github.com>
2023-08-07 11:49:11 +0800

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
-//import { useState } from "react";
 import { HomePage } from "./pages/HomePage";
 import classes from './App.module.css'
+import { SiteFooter } from "./components/SiteFooter";
 
 function App() {
- 
-
   return (
     <div className={classes.site}>
-      <header className={classes.siteHeader}>
+      <header className={`content-wrapper ${classes.siteHeader}`}>
         <h1>Brand Name </h1>     
-
         <nav>
           link1
           link2
           link3
-          link4
         </nav>
       </header>
-      <main className={classes.siteContent}>
-        <HomePage />
-          
+      <main  className={`content-wrapper ${classes.siteContent}`}>
+        <HomePage />          
       </main>
-      <footer>footer</footer>
+      <SiteFooter />
     </div>
   );
 }
---
+++ src/components/SiteFooter.tsx
@@ -0,0 +1,10 @@
+export function SiteFooter() {
+  return (
+    <footer className="content-wrapper">
+      Site Footer
+      <small>
+        - Mode: {import.meta.env.MODE} - {import.meta.env.VITE_APP_MODE_FILE}
+      </small>
+    </footer>
+  );
+}
