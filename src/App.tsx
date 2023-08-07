cb6e06df8c3fbb2f6f95e9433082e310af05a0fa
Benjie <benjie.estrella87@gmail.com>
2023-08-08 01:17:56 +0800

    3 update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { HomePage } from "./pages/HomePage";
 import classes from './App.module.css'
+import { NavbarLink } from "./components/nav";
 import { SiteFooter } from "./components/SiteFooter";
 import { AboutPage } from "./pages/AboutPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
-import { Link, Outlet, Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 
 function App() {
   // @todo implement auth required routes https://stackblitz.com/github/remix-run/react-router/tree/main/examples/auth?file=src%2FApp.tsx
@@ -11,17 +12,8 @@ function App() {
   const MainPageLayout = () => (
     <div className={classes.site}>
       <header className={`content-wrapper ${classes.siteHeader}`}>
-        <h1>Brand Name </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+        <h1 className={classes.navTitle}> Brand Name </h1>
+        <NavbarLink />
       </header>
       <main className={`content-wrapper ${classes.siteContent}`}>
         <Outlet />
