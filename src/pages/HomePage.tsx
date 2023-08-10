d9ae773c9f6bd0ee1fe5d2140e9579fdcb6820f8
Jicking Bebiro <jicking.dev@gmail.com>
2023-08-10 22:21:54 +0800

    set prettier (#7)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,15 @@
 import classes from './HomePage.module.css'
 
 export function HomePage() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum quidem
-        quisquam nihil itaque molestias amet, laudantium dolore sequi nulla
-        earum dolor sint velit nesciunt corporis, nobis minus ab magni
-        accusamus.
-      </p>
-      <button className={classes.btn}>Scoped Styled Btn</button>
-      <button className='btn'>Global Styled Btn</button>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Home Page</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum quidem quisquam nihil itaque molestias amet,
+				laudantium dolore sequi nulla earum dolor sint velit nesciunt corporis, nobis minus ab magni accusamus.
+			</p>
+			<button className={classes.btn}>Scoped Styled Btn</button>
+			<button className="btn">Global Styled Btn</button>
+		</div>
+	)
 }
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,10 +1,8 @@
 export function NotFoundPage() {
-  return (
-    <div>
-      <h1>404</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-      </p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>404</h1>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+		</div>
+	)
 }
