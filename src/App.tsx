df1492e0887ddf35db96ca2cee0cd866c5abfe8c
Benjie <benjie.estrella87@gmail.com>
2023-08-11 12:57:53 +0800

    new commit x

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,20 @@ import { AboutPage } from './pages/AboutPage'
 import { NotFoundPage } from './pages/NotFoundPage'
 import { Outlet, Route, Routes } from 'react-router-dom'
 import { LoginPage } from './pages/account/LoginPage'
+import { IoBoatSharp, IoAccessibility } from 'react-icons/io5'
 
 function App() {
 	// @todo implement auth required routes https://stackblitz.com/github/remix-run/react-router/tree/main/examples/auth?file=src%2FApp.tsx
 	// @todo extract site header to component
 	const MainPageLayout = () => (
 		<div className={classes.site}>
-			<header className={`content-wrapper ${classes.siteHeader} testing`}>
+			<header className={`content-wrapper ${classes.siteHeader} testing something testing`}>
 				<h1 className={classes.navTitle}> {import.meta.env.VITE_APP_BRAND} </h1>
 				<NavbarLink />
 			</header>
 			<main className={`content-wrapper ${classes.siteContent}`}>
+				<IoBoatSharp />
+				<IoAccessibility />
 				<Outlet />
 			</main>
 			<SiteFooter />
