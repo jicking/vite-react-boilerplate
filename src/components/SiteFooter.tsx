ad744a277c0aa3386a1ec68e85ab4ba3bc16941e
jicking <mailto:7297029+jicking@users.noreply.github.com>
2023-08-13 00:44:12 +0800

    login form

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,10 +1,8 @@
 export function SiteFooter() {
 	return (
 		<footer className="content-wrapper">
-			{import.meta.env.VITE_APP_BRAND}{' '}
-			<small>
-				- Mode: {import.meta.env.MODE} - {import.meta.env.VITE_APP_MODE_FILE}
-			</small>
+			{`${import.meta.env.VITE_APP_BRAND} - `}
+			<small>{import.meta.env.MODE}</small>
 		</footer>
 	)
 }
--- src/pages/account/LoginPage.tsx
+++ src/pages/account/LoginPage.tsx
@@ -1,14 +1,54 @@
+import { useState } from 'react'
+import classes from './LoginPage.module.css'
+import { $auth, login, logout } from '../../store/auth'
+import { useStore } from '@nanostores/react'
+
 export function LoginPage() {
+	const auth = useStore($auth)
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		login(username)
+	}
+
+	const handleClickLogout = () => {
+		logout()
+		setUsername('')
+		setPassword('')
+	}
+
 	return (
 		<div>
 			<h1>Login</h1>
 			<div>
-				<form action="">
-					<label htmlFor="username">Username</label>
-					<input type="text" id="username" />
-					<input type="button" value="Login" />
-				</form>
+				{auth?.username ? (
+					<>
+						<div>Hello {auth?.username}!</div>
+						<button onClick={handleClickLogout}>Logout</button>
+					</>
+				) : (
+					<>
+						<form action="" onSubmit={handleSubmitForm}>
+							<div className={classes[`field-container`]}>
+								<label htmlFor="username">Username</label>
+								<input type="text" id="username" required onChange={(e) => setUsername(e.target.value)} />
+							</div>
+							<div className={classes[`field-container`]}>
+								<label htmlFor="password">Password</label>
+								<input type="password" id="password" required onChange={(e) => setPassword(e.target.value)} />
+							</div>
+							<div className={classes[`field-container`]}>
+								<button type="submit" disabled={!username || !password}>
+									Login
+								</button>
+							</div>
+						</form>
+					</>
+				)}
 			</div>
+			<div></div>
 		</div>
 	)
 }
