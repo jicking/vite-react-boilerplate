675e04abdc8d1588a0d1869dd0e9fbbf64b5fb19
jicking <mailto:7297029+jicking@users.noreply.github.com>
2023-08-12 11:42:09 +0800

    e2e

---
+++ tests-e2e/navigation.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+	await page.goto('/')
+})
+
+test('Page has default title', async ({ page }) => {
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/BRAND/)
+})
+
+test('Home link', async ({ page }) => {
+	// Click the get started link.
+	await page.getByRole('link', { name: 'Home' }).click()
+
+	// Expects page to have a heading with the name of Installation.
+	await expect(page.getByRole('heading', { name: 'Home Page' })).toBeVisible()
+})
+
+test('About link', async ({ page }) => {
+	// Click the get started link.
+	await page.getByRole('link', { name: 'About' }).click()
+
+	// Expects page to have a heading with the name of Installation.
+	await expect(page.getByRole('heading', { name: 'About' })).toBeVisible()
+})
+
+test('Login link', async ({ page }) => {
+	// Click the get started link.
+	await page.getByRole('link', { name: 'Login' }).click()
+
+	// Expects page to have a heading with the name of Installation.
+	await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible()
+})
