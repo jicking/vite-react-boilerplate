f560d335f6a91c9fa20c1d70623f265a3cc2aa07
jicking <mailto:7297029+jicking@users.noreply.github.com>
2023-08-12 12:02:21 +0800

    s

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const defaultTestExclude = [
+	'**/node_modules/**',
+	'**/dist/**',
+	'**/cypress/**',
+	'**/.{idea,git,cache,output,temp}/**',
+	'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+	'**/test-e2e/**'
+]
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
-		exclude: ['test-e2e/*']
+		exclude: [...defaultTestExclude]
 	}
 })
